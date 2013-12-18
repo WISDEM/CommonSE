c6f178da9ea62ed028ee73c857d24178900a57a2
Andrew Ning <andrew.ning@nrel.gov>
2013-12-18 11:08:01 -0700

    added gradients for wind/wave/soil

--- src/commonse/environment.py
+++ src/commonse/environment.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-towerstruc.py
+environment.py
 
 Created by Andrew Ning on 2012-01-20.
 Copyright (c) NREL. All rights reserved.
@@ -10,8 +10,32 @@ Copyright (c) NREL. All rights reserved.
 import math
 import numpy as np
 from scipy.optimize import brentq
-from openmdao.main.api import Component
+from openmdao.main.api import Component, Assembly
 from openmdao.main.datatypes.api import Float, Array
+
+
+## temporary convenience method
+def check_gradient(c):
+
+    class A(Assembly):
+
+        def configure(self):
+
+            self.add('comp', c)
+            self.driver.workflow.add('comp')
+
+    a = A()
+    a.run()
+    c.linearize()
+    inputs, outputs, J = c.provideJ()
+    inputs = list(inputs)
+    outputs = list(outputs)
+    for i, inp in enumerate(inputs):
+        inputs[i] = 'comp.' + inp
+    for i, outp in enumerate(outputs):
+        outputs[i] = 'comp.' + outp
+    a.check_gradient('comp', inputs=inputs, outputs=outputs, mode='forward')
+
 
 
 # -----------------
@@ -19,7 +43,7 @@ from openmdao.main.datatypes.api import Float, Array
 # -----------------
 
 
-class Wind(Component):
+class WindBase(Component):
     """base component for wind speed/direction"""
 
     # in
@@ -30,7 +54,7 @@ class Wind(Component):
     beta = Array(iotype='out', units='deg', desc='corresponding wind angles relative to inertial coordinate system')
 
 
-class Wave(Component):
+class WaveBase(Component):
     """base component for wave speed/direction"""
 
     # in
@@ -50,7 +74,7 @@ class Wave(Component):
         self.beta = np.zeros(n)
 
 
-class Soil(Component):
+class SoilBase(Component):
     """base component for soil stiffness"""
 
     # out
@@ -65,15 +89,15 @@ class Soil(Component):
 # -----------------------
 
 
-class PowerWind(Wind):
+class PowerWind(WindBase):
     """power-law profile wind"""
 
     # variables
     Uref = Float(iotype='in', units='m/s', desc='reference velocity of power-law model')
     zref = Float(iotype='in', units='m', desc='corresponding reference height')
-    z0 = Float(0.0, iotype='in', units='m', desc='bottom of wind profile (height of ground/sea)')
 
     # parameters
+    z0 = Float(0.0, iotype='in', units='m', desc='bottom of wind profile (height of ground/sea)')
     shearExp = Float(0.2, iotype='in', desc='shear exponent')
     betaWind = Float(0.0, iotype='in', units='deg', desc='wind angle relative to inertial coordinate system')
 
@@ -102,33 +126,36 @@ class PowerWind(Wind):
         dU_dUref = np.zeros_like(z)
         dU_dz = np.zeros_like(z)
         dU_dzref = np.zeros_like(z)
-        dU_dz0 = np.zeros_like(z)
 
         idx = z > z0
         dU_dUref[idx] = ((z[idx] - z0)/(zref - z0))**self.shearExp
         dU_dz[idx] = self.U[idx]*self.shearExp * 1.0/(z[idx] - z0)
         dU_dzref[idx] = self.U[idx]*self.shearExp * -1.0/(zref - z0)
-        dU_dz0[idx] = self.U[idx]*self.shearExp * (1.0/(zref - z0) - 1.0/(z[idx] - z0))
 
-        self.J = np.array([np.hstack((dU_dUref, dU_dz, dU_dzref, dU_dz0))])
+        # TODO: the gradient at z == z0 will always be wrong, must rethink.
+
+        self.J = np.hstack((dU_dUref.reshape(-1, 1), np.diag(dU_dz), dU_dzref.reshape(-1, 1)))
 
 
     def provideJ(self):
 
-        inputs = ('Uref', 'z', 'zref', 'z0')
-        outputs = ('U')
+        inputs = ('Uref', 'z', 'zref')
+        outputs = ('U',)
 
         return inputs, outputs, self.J
 
 
 
-class LogWind(Wind):
+
+class LogWind(WindBase):
     """logarithmic-profile wind"""
 
-    # ---------- in -----------------
+    # variables
     Uref = Float(iotype='in', units='m/s', desc='reference velocity of power-law model')
     zref = Float(iotype='in', units='m', desc='corresponding reference height')
     z0 = Float(0.0, iotype='in', units='m', desc='bottom of wind profile (height of ground/sea)')
+
+    # parameters
     z_roughness = Float(10.0, iotype='in', units='mm', desc='surface roughness length')
     betaWind = Float(0.0, iotype='in', units='deg', desc='wind angle relative to inertial coordinate system')
 
@@ -144,22 +171,57 @@ class LogWind(Wind):
         # find velocity
         self.U = np.zeros_like(z)
         idx = [z > z0]
-        self.U[idx] = self.Uref*(np.log((z[idx] - z0)/z_roughness) / math.log((zref - z0)/z_roughness))
+        self.U[idx] = self.Uref*np.log((z[idx] - z0)/z_roughness) / math.log((zref - z0)/z_roughness)
         self.beta = self.betaWind*np.ones_like(z)
 
 
+    def linearize(self):
 
-class LinearWaves(Wave):
+        # rename
+        z = self.z
+        zref = self.zref
+        z0 = self.z0
+        z_roughness = self.z_roughness
+
+        dU_dUref = np.zeros_like(z)
+        dU_dz = np.zeros_like(z)
+        dU_dzref = np.zeros_like(z)
+        dU_dz0 = np.zeros_like(z)
+
+        idx = z > z0
+        lt = np.log((z[idx] - z0)/z_roughness)
+        lb = math.log((zref - z0)/z_roughness)
+        dU_dUref[idx] = lt/lb
+        dU_dz[idx] = self.Uref/lb / (z[idx] - z0)
+        dU_dzref[idx] = -self.Uref*lt / math.log((zref - z0)/z_roughness)**2 / (zref - z0)
+        dU_dz0[idx] = self.U[idx]*(-lb/(z[idx]-z0) + lt/(zref-z0))/lb**2
+
+        dU = np.hstack((dU_dUref, dU_dz, dU_dzref, dU_dz0))
+
+        self.J = np.array([dU])
+
+
+    def provideJ(self):
+
+        inputs = ('Uref', 'z', 'zref', 'z0')
+        outputs = ('U',)
+
+        return inputs, outputs, self.J
+
+
+class LinearWaves(WaveBase):
     """linear (Airy) wave theory"""
 
-    # ---------- in -------------
+    # variables
+    Uc = Float(iotype='in', units='m/s', desc='mean current speed')
+
+    # parameters
+    z_surface = Float(iotype='in', units='m', desc='vertical location of water surface')
     hs = Float(iotype='in', units='m', desc='significant wave height (crest-to-trough)')
     T = Float(iotype='in', units='s', desc='period of waves')
-    g = Float(9.81, iotype='in', units='m/s**2', desc='acceleration of gravity')
-    Uc = Float(iotype='in', units='m/s', desc='mean current speed')
-    betaWave = Float(0.0, iotype='in', units='deg', desc='wave angle relative to inertial coordinate system')
-    z_surface = Float(iotype='in', units='m', desc='vertical location of water surface')
     z_floor = Float(0.0, iotype='in', units='m', desc='vertical location of sea floor')
+    g = Float(9.81, iotype='in', units='m/s**2', desc='acceleration of gravity')
+    betaWave = Float(0.0, iotype='in', units='deg', desc='wave angle relative to inertial coordinate system')
 
 
     def execute(self):
@@ -191,17 +253,33 @@ class LinearWaves(Wave):
         # angles
         self.beta = self.betaWave*np.ones_like(self.z)
 
+        # derivatives
+        dU_dz = h/2.0*omega*np.sinh(k*(z_rel + d))/math.sinh(k*d)*k
+        dU_dUc = 1.0
+        dA_dz = omega*dU_dz
+        dA_dUc = omega
+        self.J = np.vstack([np.array([dU_dz, dU_dUc]), np.array([dA_dz, dA_dUc])])
 
-class TowerSoil(Soil):
+    def provideJ(self):
+
+        inputs = ('z', 'Uc')
+        outputs = ('U', 'A')
+
+        return inputs, outputs, self.J
+
+
+class TowerSoil(SoilBase):
     """textbook soil stiffness method"""
 
-    # in
+    # variable
+    r0 = Float(1.0, iotype='in', units='m', desc='radius of base of tower')
+    depth = Float(1.0, iotype='in', units='m', desc='depth of foundation in the soil')
+
+    # parameter
     G = Float(140e6, iotype='in', units='Pa', desc='shear modulus of soil')
     nu = Float(0.4, iotype='in', desc='Poisson''s ratio of soil')
-    depth = Float(1.0, iotype='in', units='m', desc='depth of foundation in the soil')
     rigid = Array(iotype='in', dtype=np.bool, desc='directions that should be considered infinitely rigid\
         order is x, theta_x, y, theta_y, z, theta_z')
-    r0 = Float(1.0, iotype='in', units='m', desc='radius of base of tower')
 
 
     def execute(self):
@@ -230,3 +308,82 @@ class TowerSoil(Soil):
         self.k[self.rigid] = float('inf')
 
 
+    def linearize(self):
+
+        G = self.G
+        nu = self.nu
+        h = self.depth
+        r0 = self.r0
+
+        # vertical
+        eta = 1.0 + 0.6*(1.0-nu)*h/r0
+        deta_dr0 = -0.6*(1.0-nu)*h/r0**2
+        dkz_dr0 = 4*G/(1.0-nu)*(eta + r0*deta_dr0)
+
+        deta_dh = -0.6*(1.0-nu)/r0
+        dkz_dh = 4*G*r0/(1.0-nu)*deta_dh
+
+        # horizontal
+        eta = 1.0 + 0.55*(2.0-nu)*h/r0
+        deta_dr0 = -0.55*(2.0-nu)*h/r0**2
+        dkx_dr0 = 32.0*(1.0-nu)*G/(7.0-8.0*nu)*(eta + r0*deta_dr0)
+
+        deta_dh = 0.55*(2.0-nu)/r0
+        dkx_dh = 32.0*(1.0-nu)*G*r0/(7.0-8.0*nu)*deta_dh
+
+        # rocking
+        eta = 1.0 + 1.2*(1.0-nu)*h/r0 + 0.2*(2.0-nu)*(h/r0)**3
+        deta_dr0 = -1.2*(1.0-nu)*h/r0**2 - 3*0.2*(2.0-nu)*(h/r0)**3/r0
+        dkthetax_dr0 = 8.0*G/(3.0*(1.0-nu))*(3*r0**2*eta + r0**3*deta_dr0)
+
+        deta_dh = 1.2*(1.0-nu)/r0 + 3*0.2*(2.0-nu)*(1.0/r0)**3*h**2
+        dkthetax_dh = 8.0*G*r0**3/(3.0*(1.0-nu))*deta_dh
+
+        # torsional
+        dkphi_dr0 = 16.0*G*3*r0**2/3.0
+        dkphi_dh = 0.0
+
+        dk_dr0 = np.array([dkx_dr0, dkthetax_dr0, dkx_dr0, dkthetax_dr0, dkz_dr0, dkphi_dr0])
+        dk_dr0[self.rigid] = 0.0
+        dk_dh = np.array([dkx_dh, dkthetax_dh, dkx_dh, dkthetax_dh, dkz_dh, dkphi_dh])
+        dk_dh[self.rigid] = 0.0
+
+        self.J = np.vstack((dk_dr0, dk_dh))
+
+
+    def provideJ(self):
+
+        inputs = ('r0', 'depth')
+        outputs = ('k',)
+
+        return inputs, outputs, self.J
+
+
+
+if __name__ == '__main__':
+    p = PowerWind()
+    p.Uref = 10.0
+    p.zref = 100.0
+    p.z0 = 1.0
+    p.z = np.linspace(1.0, 120.0, 20)
+    p.shearExp = 0.2
+    p.betaWind = 0.0
+    # check_gradient(p)
+
+    p.run()
+    U0 = p.U[0]
+
+    p.z[0] += 1e-6
+    p.run()
+    print p.U[0]
+    print U0
+    print (p.U[0] - U0)/1e-6
+
+    # l = LogWind()
+    # l.Uref = 12.0
+    # l.zref = 100.0
+    # l.z0 = 5.0
+
+    # # parameters
+    # z_roughness = Float(10.0, iotype='in', units='mm', desc='surface roughness length')
+    # betaWind = Float(0.0
