11860407ed1194f9ae303b2cd919ce25fc63ee64
Rick Damiani <rick.damiani@nrel.gov>
2014-01-04 12:15:16 -0700

    slight mod to RigidMember.py

--- src/commonse/__init__.py
+++ src/commonse/__init__.py
@@ -1,6 +1,6 @@
 from csystem import DirectionVector
 from utilities import cosd, sind, tand, Vector
-from SegIntersect import SegIntersect
+from SegIntersect import SegIntersect, CalcDist
 from Material import Material
 from Tube import Tube
 
