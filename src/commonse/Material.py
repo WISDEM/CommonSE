8525ef75079c80d3ee7b391584c45e4575cdfee5
Rick Damiani <rick.damiani@nrel.gov>
2014-05-19 06:58:19 -0600

    modified a few things in Material

--- src/commonse/Material.py
+++ src/commonse/Material.py
@@ -13,7 +13,7 @@ def main():
     """Material Class"""
     #The main fct is written to test the class
     mat=Material(E=3.5e5,rho=8500)
-    mat1=Material(name='grout')
+    mat1=Material(matname='grout')
     print 'Example Returning mat and mat1 as 2 objects'
     return mat,mat1
 
@@ -21,7 +21,7 @@ def main():
 class Material: #isotropic for the time being
     def __init__(self,**kwargs):
 
-        prms={'name':'ASTM992_steel','E':2.1e11,'nu':0.33,'rho':8502., 'fy':345.e6, 'fyc':345.e6} #SI Units
+        prms={'matname':'ASTM992_steel','E':2.1e11,'nu':0.33,'rho':8502., 'fy':345.e6, 'fyc':345.e6} #SI Units
         prms.update(kwargs) #update in case user put some new params in
         for key in prms:  #Initialize material object with these parameters, possibly updated by user' stuff
             setattr(self,key,prms[key])
@@ -29,23 +29,23 @@ class Material: #isotropic for the time being
         #Fill in the rest of the fields user may have skipped
 
         #Predefined materials
-        steel={'name':'ASTM992_steel',          'E':2.1e11,'nu':0.33,'rho':7805., 'fy':345.e6, 'fyc':345.e6,} #SI Units
-        heavysteel={'name':'ASTM992_steelheavy','E':2.1e11,'nu':0.33,'rho':8741., 'fy':345.e6, 'fyc':345.e6} #SI Units
-        grout={'name':'Grout',                  'E':3.9e10,'nu':0.33,'rho':2500., 'fy':20.68e6, 'fyc':20.68e6} #SI Units  TO REVISE FOR GROUT
+        steel={'matname':'ASTM992_steel',          'E':2.1e11,'nu':0.33,'rho':7805., 'fy':345.e6, 'fyc':345.e6,} #SI Units
+        heavysteel={'matname':'ASTM992_steelheavy','E':2.1e11,'nu':0.33,'rho':8741., 'fy':345.e6, 'fyc':345.e6} #SI Units
+        grout={'matname':'Grout',                  'E':3.9e10,'nu':0.33,'rho':2500., 'fy':20.68e6, 'fyc':20.68e6} #SI Units  TO REVISE FOR GROUT
 
-        if ((prms['name'].lower() == 'steel') or (prms['name'].lower() == 'astm992_steel')  or (prms['name'].lower() == '')):
+        if ((prms['matname'].lower() == 'steel') or (prms['matname'].lower() == 'astm992_steel')  or (prms['matname'].lower() == '')):
 
             for key in prms:
                 if not(key in kwargs) or (prms[key]==[]):  #I need to operate on parameters not set by user
                     setattr(self,key,steel[key])
 
-        elif ((prms['name'].lower() == 'heavysteel') or (prms['name'].lower() == 'astm992_steelheavy') or (prms['name'].lower() == 'heavysteel')):
+        elif ((prms['matname'].lower() == 'heavysteel') or (prms['matname'].lower() == 'astm992_steelheavy') or (prms['matname'].lower() == 'heavysteel')):
 
             for key in prms:
                 if not(key in kwargs) or (prms[key]==[]):  #I need to operate on parameters not set by user
                     setattr(self,key,heavysteel[key])
 
-        elif ((prms['name'].lower() == 'concrete') or (prms['name'].lower() == 'grout')):
+        elif ((prms['matname'].lower() == 'concrete') or (prms['matname'].lower() == 'grout')):
 
             for key in prms:
                 if not(key in kwargs) or (prms[key]==[]):
