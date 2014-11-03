dee04762f1b40b33ecd57a2e44481c7386ef5e52
Rick Damiani <rick.damiani@nrel.gov>
2014-11-03 14:44:04 -0700

    Enhanced for user-input G value.

--- src/commonse/Material.py
+++ src/commonse/Material.py
@@ -21,7 +21,7 @@ def main():
 class Material: #isotropic for the time being
     def __init__(self,**kwargs):
 
-        prms={'matname':'ASTM992_steel','E':2.1e11,'nu':0.33,'rho':8502., 'fy':345.e6, 'fyc':345.e6} #SI Units
+        prms={'matname':'ASTM992_steel','E':2.1e11,'nu':0.33,'G':7.895e10, 'rho':8502., 'fy':345.e6, 'fyc':345.e6} #SI Units
         prms.update(kwargs) #update in case user put some new params in
         for key in prms:  #Initialize material object with these parameters, possibly updated by user' stuff
             setattr(self,key,prms[key])
@@ -29,9 +29,9 @@ class Material: #isotropic for the time being
         #Fill in the rest of the fields user may have skipped
 
         #Predefined materials
-        steel={'matname':'ASTM992_steel',          'E':2.1e11,'nu':0.33,'rho':7805., 'fy':345.e6, 'fyc':345.e6,} #SI Units
-        heavysteel={'matname':'ASTM992_steelheavy','E':2.1e11,'nu':0.33,'rho':8741., 'fy':345.e6, 'fyc':345.e6} #SI Units
-        grout={'matname':'Grout',                  'E':3.9e10,'nu':0.33,'rho':2500., 'fy':20.68e6, 'fyc':20.68e6} #SI Units  TO REVISE FOR GROUT
+        steel={'matname':'ASTM992_steel',          'E':2.1e11,'nu':0.33,'G':7.895e10,'rho':7805., 'fy':345.e6, 'fyc':345.e6,} #SI Units
+        heavysteel={'matname':'ASTM992_steelheavy','E':2.1e11,'nu':0.33,'G':7.895e10,'rho':8741., 'fy':345.e6, 'fyc':345.e6} #SI Units
+        grout={'matname':'Grout',                  'E':3.9e10,'nu':0.33,'G':1.466e10,'rho':2500., 'fy':20.68e6,'fyc':20.68e6} #SI Units  TO REVISE FOR GROUT
 
         if ((prms['matname'].lower() == 'steel') or (prms['matname'].lower() == 'astm992_steel')  or (prms['matname'].lower() == '')):
 
@@ -51,8 +51,10 @@ class Material: #isotropic for the time being
                 if not(key in kwargs) or (prms[key]==[]):
                     setattr(self,key,grout[key])
 
-        self.G=self.E/(2.*(1.+self.nu))  #isotropic
-
+        if not(hasattr(self,'G')) or not(self.G):
+            self.G=self.E/(2.*(1.+self.nu))  #isotropic
+        else: #if G is given then recalculate nu
+            self.nu=self.E/(2.*self.G)-1.
 
 if __name__ == '__main__':
     mat,mat1=main()
